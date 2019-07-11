d60a76d5abcfa8881cd06a9019baa70739c86f60
AC03582 <AC03582@5CG44506GQ.ctl.intranet>
2019-07-11 16:02:57 +0530

    init commit

---
+++ springBootChatApp/src/main/java/app/springboot/SpringBootChatApp.java
@@ -0,0 +1,13 @@
+package app.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChatApp.class, args);
+	}
+
+}
---
+++ springBootChatApp/src/main/java/app/springboot/model/ChatMessage.java
@@ -0,0 +1,32 @@
+package app.springboot.model;
+
+public class ChatMessage {
+	
+	public enum MessageType{
+		CHAT,LEAVE,JOIN
+	}
+	
+	private String content;
+	private String sender;
+	private MessageType type;
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public MessageType getType() {
+		return type;
+	}
+	public void setType(MessageType type) {
+		this.type = type;
+	}
+	
+
+}
