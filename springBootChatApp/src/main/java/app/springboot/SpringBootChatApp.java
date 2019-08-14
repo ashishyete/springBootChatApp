3673d6709c5f0b4ef73872c85d4c136f9e835769
Ashish Yete <ashish.yete@gmail.com>
2019-08-14 11:53:48 +0530

    Update SpringBootChatApp.java
    added spring security

--- springBootChatApp/src/main/java/app/springboot/SpringBootChatApp.java
+++ springBootChatApp/src/main/java/app/springboot/SpringBootChatApp.java
@@ -2,8 +2,10 @@ package app.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class SpringBootChatApp {
 
 	public static void main(String[] args) {
